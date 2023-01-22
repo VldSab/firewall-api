9e23e3414af075be4b85094892855b0db33a7645
VldSab <vladimir.saber@gmail.com>
2023-01-22 20:25:12 +0300

    Initialisation web-face on React

---
+++ web-face/src/scenes/theme.js
@@ -0,0 +1,10 @@
+import { createContext, useState, useMemo } from "react";
+import { createTheme } from "@mui/material/styles";
+
+//color design tockens
+
+#666666
+#141b2d
+#4cceac
+#db4f4a
+№6870fa
