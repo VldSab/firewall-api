a6b967645535adc0147f856300b80011d406f749
VladimirSaberullin <saberullin.vld@yandex.ru>
2023-02-14 14:18:18 +0300

    Перенос web-face

--- web-face/src/scenes/dashboard/index.jsx
+++
@@ -1,9 +0,0 @@
-import Header from "../../components/Header";
-
-const Dashboard = () => {
-    return <div>
-        <Header title="DASHBOARD" subtitle="Manage your Liberty Firewall"/>
-    </div>;
-}
-
-export default Dashboard;
